09705472ab1be9228983371fa81706527763c271
Gregory Ojiem <gro3228@rit.edu>
2024-06-04 06:03:10 -0400

    New functions for text rendering

--- src/utility/GraphicsUtil.h
+++ src/utility/GraphicsUtil.h
@@ -3,12 +3,15 @@
 
 #include <vector>
 
-#include "../game/Block.h"
-
-struct ChunkVertex;
+#include "../rendering/Vertex.h"
 
 extern std::vector<ChunkVertex> generateBlockVertices(Block block);
 
 extern std::vector<uint32_t> generateBlockIndices(uint32_t startIndex);
+
+extern std::vector<TexturedVertex> generateTexturedQuad(glm::vec4 quadBounds, glm::vec4 texQuadBounds,
+    glm::vec2 startPos, float scale);
+
+extern std::vector<uint32_t> generateTexturedQuadIndices(uint32_t startIndex);
 
 #endif //GRAPHICSUTIL_H
