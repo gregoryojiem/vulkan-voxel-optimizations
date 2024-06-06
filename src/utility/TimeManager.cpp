3f4c1639107f371b62ba233435d424e69071477c
Gregory Ojiem <gro3228@rit.edu>
2024-06-05 23:54:33 -0400

    Added a timer

--- src/utility/TimeManager.cpp
+++ src/utility/TimeManager.cpp
@@ -1,20 +1,33 @@
-//
-// Created by Gregory on 5/25/2024.
-//
-
 #include "TimeManager.h"
 
-std::chrono::time_point<std::chrono::high_resolution_clock> TimeManager::lastTime = std::chrono::high_resolution_clock::now();
+std::chrono::time_point<std::chrono::high_resolution_clock> TimeManager::lastFrameTime = std::chrono::high_resolution_clock::now();
 float TimeManager::deltaTime;
+std::chrono::time_point<std::chrono::high_resolution_clock> TimeManager::startTime;
+bool TimeManager::timerRunning;
 
 float TimeManager::setDeltaTime() {
     auto currentTime = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> newDeltaTime = currentTime - lastTime;
-    lastTime = currentTime;
+    std::chrono::duration<float> newDeltaTime = currentTime - lastFrameTime;
+    lastFrameTime = currentTime;
     deltaTime = newDeltaTime.count();
     return deltaTime;
 }
 
 float TimeManager::getDeltaTime() {
     return deltaTime;
+}
+
+void TimeManager::startTimer() {
+    timerRunning = true;
+    startTime = std::chrono::high_resolution_clock::now();
+}
+
+float TimeManager::finishTimer() {
+    if (!timerRunning) {
+        return 0.0f;
+    }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> elapsed = endTime - startTime;
+    timerRunning = false;
+    return elapsed.count();
 }
--- src/utility/TimeManager.h
+++ src/utility/TimeManager.h
@@ -8,9 +8,14 @@ public:
     static float setDeltaTime();
     static float getDeltaTime();
 
+    static void startTimer();
+    static float finishTimer();
+
 private:
-    static std::chrono::time_point<std::chrono::high_resolution_clock> lastTime;
+    static std::chrono::time_point<std::chrono::high_resolution_clock> lastFrameTime;
     static float deltaTime;
+    static std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+    static bool timerRunning;
 };
 
 #endif //TIMEMANAGER_H
