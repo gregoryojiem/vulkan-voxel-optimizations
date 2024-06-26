da0cdef9271ed569d567f8a5eeb2b4ad4ec2795a
Gregory Ojiem <gro3228@rit.edu>
2024-06-26 11:21:20 -0400

    Movement increase for larger worlds

--- src/rendering/misc/Camera.cpp
+++ src/rendering/misc/Camera.cpp
@@ -18,7 +18,7 @@ void Camera::init(GLFWwindow *window, uint32_t width, uint32_t height) {
     yaw = 0.0f;
     pitch = 0.0f;
     fovy = 45.0f;
-    movementSpeed = 30.0f;
+    movementSpeed = 100.0f;
     horzMouseSens = 200.0f;
     vertMouseSens = 200.0f;
 
