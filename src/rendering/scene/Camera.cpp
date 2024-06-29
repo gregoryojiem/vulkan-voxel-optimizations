e991dbf9988f51d48f8b2f869a060a110d90f958
Gregory Ojiem <gro3228@rit.edu>
2024-06-29 11:00:26 -0400

    Refactored Camera for clarity
    Camera now stores more of the important parameters, and makes much more sense in many areas. Much of the input handling and yaw/pitch calculations have been reworked, and the front vector calculation has been fixed to account for the coordinate system

--- src/rendering/scene/Camera.cpp
+++ src/rendering/scene/Camera.cpp
@@ -5,37 +5,41 @@
 #include <cstring>
 #include <iostream>
 #include <../../dependencies/glm-1.0.1/glm/glm.hpp>
-#include <../../dependencies/glm-1.0.1/glm/gtc/matrix_transform.hpp>
 
-#include "../../util/InputManager.h"
 #include "../vulkan/VulkanStructs.h"
-
-UniformBufferObject Camera::ubo;
 
 void Camera::init(GLFWwindow *window, uint32_t width, uint32_t height) {
     position = glm::vec3(0.0f, 0.0f, 5.0f);
     up = glm::vec3(0.0f, 1.0f, 0.0f);
     yaw = 0.0f;
     pitch = 0.0f;
-    fovy = 45.0f;
+    fovY = 45.0f;
+    zNear = 0.1f;
+    zFar = 1000000.0f;
     movementSpeed = 100.0f;
     horzMouseSens = 200.0f;
     vertMouseSens = 200.0f;
+    aspect = static_cast<float>(width) / static_cast<float>(height);
 
-    float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
     ubo.model = glm::mat4(1.0f);
-    ubo.view = glm::lookAt(position, glm::vec3(0.0f, 2.0f, 0.0f), up);
-    ubo.proj = glm::perspective(glm::radians(fovy), aspectRatio, 0.1f, 10000.0f);
+    ubo.view = glm::lookAt(position, glm::vec3(0.0f, 0.0f, 0.0f), up);
+    ubo.proj = glm::perspective(glm::radians(fovY), aspect, zNear, zFar);
     ubo.proj[1][1] *= -1;
     InputHandler::init(window);
 }
 
 
 void Camera::update(float deltaTime) {
-    InputState state = InputHandler::getState();
+    const InputState state = InputHandler::getState();
+    handleMouseInput(state, deltaTime);
+    const glm::vec3 front = getFrontVec();
+    const glm::vec3 movement = handleKeyboardInput(state, front, deltaTime);
+    position += movement * movementSpeed;
+    ubo.view = glm::lookAt(position, position + front, up);
+}
 
-    // handle input and calculate new yaw and pitch to get the new front vector
-    yaw += -static_cast<float>(state.xDelta) * horzMouseSens * deltaTime;
+void Camera::handleMouseInput(const InputState &state, float deltaTime) {
+    yaw += static_cast<float>(state.xDelta) * horzMouseSens * deltaTime;
     pitch += static_cast<float>(state.yDelta) * vertMouseSens * deltaTime;
     if (pitch > 89.0f) {
         pitch = 89.0f;
@@ -43,39 +47,35 @@ void Camera::update(float deltaTime) {
     if (pitch < -89.0f) {
         pitch = -89.0f;
     }
-
     yaw = fmodf(yaw, 360);
-
     InputHandler::resetCursor();
+}
 
-    glm::vec3 front;
-    front.x = static_cast<float>(glm::sin(glm::radians(yaw)) * cos(glm::radians(pitch)));
-    front.y = glm::sin(glm::radians(pitch));
-    front.z = static_cast<float>(glm::cos(glm::radians(yaw)) * cos(glm::radians(pitch)));
-    front = glm::normalize(front);
-
-    glm::vec3 right = glm::normalize(glm::cross(front, up));
-
-    // change position and update camera
-
+glm::vec3 Camera::handleKeyboardInput(const InputState &state, const glm::vec3 &front, float deltaTime) const {
+    glm::vec3 right = glm::normalize(glm::cross(up, front));
     auto movement = glm::vec3(0.0f);
-
-    if (state.wState) movement -= front;
-    if (state.sState) movement += front;
+    if (state.wState) movement += front;
+    if (state.sState) movement -= front;
     if (state.aState) movement += right;
     if (state.dState) movement -= right;
     if (state.spaceState) movement += up;
     if (state.shiftState) movement -= up;
-
     if (length(movement) > 0.0f) {
         movement = glm::normalize(movement);
     }
-
-    position += movement * movementSpeed * deltaTime;
-    ubo.view = glm::lookAt(position, position - front, up);
+    return movement * deltaTime;
 }
 
-void Camera::updateProj(uint32_t width, uint32_t height) const {
-    float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
-    ubo.proj = glm::perspective(glm::radians(fovy), aspectRatio, 0.1f, 300.0f);
+void Camera::updateAspect(uint32_t width, uint32_t height) {
+    aspect = static_cast<float>(width) / static_cast<float>(height);
+    ubo.proj = glm::perspective(glm::radians(fovY), aspect, zNear, zFar);
+    ubo.proj[1][1] *= -1;
+}
+
+glm::vec3 Camera::getFrontVec() const {
+    glm::vec3 front;
+    front.x = static_cast<float>(glm::sin(glm::radians(yaw)) * cos(glm::radians(pitch)));
+    front.y = glm::sin(glm::radians(pitch));
+    front.z = -static_cast<float>(glm::cos(glm::radians(yaw)) * cos(glm::radians(pitch)));
+    return glm::normalize(front);
 }
--- src/rendering/scene/Camera.h
+++ src/rendering/scene/Camera.h
@@ -1,30 +1,52 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include "../vulkan/VulkanStructs.h"
 #include "GLFW/glfw3.h"
+#include "../vulkan/VulkanStructs.h"
+#include "../../util/InputManager.h"
+
+struct Plane {
+    glm::vec3 normal;
+    glm::vec3 distFromOrigin;
+};
+
+struct Frustrum {
+    Plane bottom;
+    Plane top;
+    Plane left;
+    Plane right;
+    Plane near;
+    Plane far;
+};
 
 class Camera {
 public:
-    static UniformBufferObject ubo;
+    UniformBufferObject ubo;
 
     void init(GLFWwindow *window, uint32_t width, uint32_t height);
 
     void update(float deltaTime);
 
-    void updateProj(uint32_t width, uint32_t height) const;
+    void handleMouseInput(const InputState &state, float deltaTime);
+
+    [[nodiscard]] glm::vec3 handleKeyboardInput(const InputState &state, const glm::vec3 &front, float deltaTime) const;
+
+    void updateAspect(uint32_t width, uint32_t height);
 
 private:
     glm::vec3 position{};
-    glm::vec3 front{};
     glm::vec3 up{};
-    glm::vec3 right{};
     float yaw{};
     float pitch{};
-    float fovy{};
+    float fovY{};
+    float aspect{};
+    float zNear{};
+    float zFar{};
     float movementSpeed{};
     float horzMouseSens{};
     float vertMouseSens{};
+
+    [[nodiscard]] glm::vec3 getFrontVec() const;
 };
 
 #endif //CAMERA_H
